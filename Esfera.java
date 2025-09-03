29b7e954e979d21032dac7c33f6ffe1f06b33edb
Fran ʚĭɞ <134552777+BatataDeCodigos@users.noreply.github.com>
2025-09-03 08:43:55 -0300

    geo espacial do vinny

---
+++ Esfera.java
@@ -0,0 +1,17 @@
+
+public class Esfera {
+
+    public Esfera(double raio) {
+        //TODO Auto-generated constructor stub
+    }
+
+    String volume() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public String areaSuperficial() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'areaSuperficial'");
+    }
+
+}
--- geo_plana.java
+++ geo_plana.java
@@ -1,5 +1,22 @@
-public class geo_plana {
+import java.util.Scanner;
 
+public class geo_plana {
     
+    //circulo
+    public static void circulo(Scanner sc){
+        //trazer o raio
+        System.out.print("Digite o raio desejado: \n");
+        double raio = sc.nextDouble();
+        System.out.println("Área do círculo é "+(Math.PI*raio*raio));
+    
+    }
+//quadrado
+    public static void quadrado(Scanner sc){
+
+        System.out.print("Digite o lado desejado: \n");
+        double lado = sc.nextDouble();
+        System.out.println("Área do quadrado é "+(lado*lado));
+
+    }
     
 }
