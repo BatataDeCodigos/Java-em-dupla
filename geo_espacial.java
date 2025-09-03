7e6e7d4433ff6273388a467b12f5dbf6c93bea46
Fran ʚĭɞ <134552777+BatataDeCodigos@users.noreply.github.com>
2025-09-03 08:49:22 -0300

    Mostra de erros - Corrigir em casa

--- geo_espacial.java
+++ geo_espacial.java
@@ -3,28 +3,9 @@ import java.util.Scanner;
 public class geo_espacial {
 //Vinny está com problemas de identificação de programa; aqui foi oque ele fez
 
-    // cubo
-    public static void cubo(Scanner sc) {
-        System.out.print("Digite o lado do cubo: ");
-        double lado = sc.nextDouble();
-        double volume = Math.pow(lado, 3);
-        double area = 6 * Math.pow(lado, 2);
 
-        System.out.println("Volume do cubo = " + volume);
-        System.out.println("Área superficial do cubo = " + area);
+
     }
-
-    // esfera
-    public static void esfera(Scanner sc) {
-        System.out.print("Digite o raio da esfera: ");
-        double raio = sc.nextDouble();
-
-        Esfera esfera = new Esfera(raio);
-
-        System.out.println("Volume da esfera = " + esfera.volume());
-        System.out.println("Área da esfera = " + esfera.areaSuperficial());
-    }
-}
 
         
 
