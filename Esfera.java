d7b220fbc23208ea348ca63b40027df8774d4b67
FraNFraN <134552777+BatataDeCodigos@users.noreply.github.com>
2025-09-04 17:51:04 -0300

    tentando consertar o main

--- Esfera.java
+++ Esfera.java
@@ -1,17 +1,6 @@
 
-public class Esfera {
+public class esfera {
 
-    public Esfera(double raio) {
-        //TODO Auto-generated constructor stub
-    }
 
-    String volume() {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    public String areaSuperficial() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'areaSuperficial'");
-    }
 
 }
--- geo_espacial.java
+++ geo_espacial.java
@@ -3,9 +3,19 @@ import java.util.Scanner;
 public class geo_espacial {
 //Vinny está com problemas de identificação de programa; aqui foi oque ele fez
 
-
-
+    public static void cubo(Scanner sc) {
+        System.out.print("Digite o lado: ");
+        double lado = sc.nextDouble();
+        System.out.println("Volume do cubo = " + Math.pow(lado, 3));
     }
+
+    public static void esfera(Scanner sc) {
+        System.out.print("Digite o raio: ");
+        double r = sc.nextDouble();
+        System.out.println("Volume da esfera = " + (4.0 / 3.0 * Math.PI * Math.pow(r, 3)));
+    }
+}
+
 
         
 
